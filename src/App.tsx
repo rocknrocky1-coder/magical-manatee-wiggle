2f45c7d5212af6ddebc6fc849aa93c9fe3381f1a
Dyad <git@dyad.sh>
2026-08-17 23:09:06 +0530

    Removed unnecessary QueryClient and QueryClientProvider to fix 'QueryClient is not defined' error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import { Toaster } from "@/components/ui/toaster"; 
+import { TooltipProvider } from "@/components/ui/tooltip"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import Index from "./pages/Index"; 
+import NotFound from "./pages/NotFound"; 
 import { EcommerceProvider } from "./context/EcommerceContext";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <EcommerceProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/category/*" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </EcommerceProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => ( 
+  <TooltipProvider> 
+    <Toaster /> 
+    <EcommerceProvider> 
+      <BrowserRouter> 
+        <Routes> 
+          <Route path="/" element={<Index />} /> 
+          <Route path="/category/*" element={<Index />} /> 
+          <Route path="*" element={<NotFound />} /> 
+        </Routes> 
+      </BrowserRouter> 
+    </EcommerceProvider> 
+  </TooltipProvider> 
+); 
 
 export default App;
