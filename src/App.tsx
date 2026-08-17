10ba936aa99c06c7658cc9048a7e09e5064477c7
Dyad <git@dyad.sh>
2026-08-17 22:51:32 +0530

    wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -16,7 +16,6 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            {/* Category routes - render Index for any /category/* path */}
             <Route path="/category/*" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
